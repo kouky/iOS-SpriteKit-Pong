ceb217350d3c7e445df245fab653bc41e860ac12
Michael Koukoullis <mkoukoullis@gmail.com>
2013-08-23 18:06:30 +1000

    Refactor PaddleNodeContactVisitor

--- Pong/PaddleNodeContactVisitor.m
+++ Pong/PaddleNodeContactVisitor.m
@@ -22,13 +22,14 @@
     CGPoint point = [paddle convertPoint:self.contact.contactPoint fromNode:ball.scene];
     CGPoint normalisedPoint = [paddle normalisePoint:point];
     
-    // Interpolate the ball reflection angle based on the vertical contact point with paddle
-    // Cap the reflection at 45 degrees
-    float interpolatedReflectionAngle = M_PI_4 * normalisedPoint.y;
-    if (interpolatedReflectionAngle > 0.5)
-        interpolatedReflectionAngle = 0.5;
-    else if (interpolatedReflectionAngle < -0.5)
-        interpolatedReflectionAngle = -0.5;
+    // Interpolate the ball reflection angle based on the vertical contact point with the paddle
+    // Cap the reflection a90 degress arc from the center of the paddles
+    float interpolatedReflectionAngle = 0;
+    if ([paddle.name isEqualToString:@"leftPaddle"]) {
+        interpolatedReflectionAngle = M_PI_2 - (normalisedPoint.y * M_PI_4);
+    } else if ([paddle.name isEqualToString:@"rightPaddle"]) {
+        interpolatedReflectionAngle = M_PI + M_PI_2 + (normalisedPoint.y * M_PI_4);
+    }
     
     // Determine the velocity magnitude for the ball based on the
     // speed of the paddle on contact.
@@ -40,18 +41,8 @@
 
     float velocityMagnitude = 300 + 500 * paddleSpeedNormalised;
     
-    // Determine the ball velocity component vectors
-    float horizontalVelocity = velocityMagnitude * cos(interpolatedReflectionAngle);
-    float verticalVelocity = velocityMagnitude * sin(interpolatedReflectionAngle);
-    
-    // Make sure the horizintal velocity for the ball is reflected
-    if ([paddle.name isEqualToString:@"leftPaddle"])
-        horizontalVelocity = fabsf(horizontalVelocity);
-    else if ([paddle.name isEqualToString:@"rightPaddle"])
-        horizontalVelocity = -1 * fabsf(horizontalVelocity);
-    
     // Set the ball velocity
-    ballBody.velocity = CGVectorMake(horizontalVelocity, verticalVelocity);
+    [ball setVelocityWithRadians:interpolatedReflectionAngle Magnitude:velocityMagnitude];
 }
 
 @end
